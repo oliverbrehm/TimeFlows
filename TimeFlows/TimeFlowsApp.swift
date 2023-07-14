a170242e142b8e1e010d78c628b8dd532088abe5
Oliver Brehm <brehm@mobile-software.ag>
2023-07-14 17:23:41 +0200

    Initial Commit

---
+++ TimeFlows/TimeFlowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeFlowsApp.swift
+//  TimeFlows
+//
+//  Created by Oliver Brehm on 14.07.23.
+//
+
+import SwiftUI
+
+@main
+struct TimeFlowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
